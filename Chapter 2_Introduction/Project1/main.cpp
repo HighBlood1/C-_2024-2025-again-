9931626b4c4404c42963e64c4f1aa01cb0194054
HB <jeysongmr@gmail.com>
2024-12-22 21:56:36 -0800

    First program -Welcome to C++ -

---
+++ Chapter 2_Introduction/Project1/main.cpp
@@ -0,0 +1,13 @@
+//First program 
+//Text printing program
+#include<iostream> //allows program to output data to the screen
+
+//function main bigins program execution
+int main() {
+	
+	std::cout << "Welcome to C++\n"; //display message 
+
+	return 0; //indicate that program ended successfully
+
+}//end fuction main
+
